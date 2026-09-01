3a933e1187b1c53341a52da9d65285185abe758e
Rafaeldo15 <rafaelosilva61@gmail.com>
2026-08-31 21:42:58 -0300

    adicionando atualizações

--- PythonProject/CURSO_PYTHON/questao_6.py
+++ PythonProject/CURSO_PYTHON/questao_6.py
@@ -2,3 +2,6 @@ X=(int("15"))
 y=(int("30"))
 print("x=", y)
 print("y=", X)
+
+
+#Como a regra é não usar novamente o número, usei a variável e só inverti o print
---
+++ PythonProject/CURSO_PYTHON/questao_7.py
@@ -0,0 +1,3 @@
+produto= (float("250.50"))
+resultado = produto - 15%
+print(resultado)
