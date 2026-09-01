62d3a3cc9f9ffc35646d75e19868246c6044b099
Rafaeldo15 <rafaelosilva61@gmail.com>
2026-09-01 19:25:47 -0300

    adicionando atualizações

---
+++ PythonProject/questao_5.py
@@ -0,0 +1,7 @@
+ano_1= int(input("digite o ano do seu nascimento:"))
+ano_2 = int(2026)
+resultado = ano_2 - ano_1
+print (f"sua idade é {resultado} anos")
+
+
+#há de adicionar o "int" para númrero inteiro
