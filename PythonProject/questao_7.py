868cd33e1a75fd6c673ce1176ca951271d109258
Rafaeldo15 <rafaelosilva61@gmail.com>
2026-09-01 20:18:17 -0300

    finalização dos ultimos exercícios

--- PythonProject/questao_7.py
+++ PythonProject/questao_7.py
@@ -1,3 +1,10 @@
-produto= (float("250.50"))
-resultado = produto - 15%
-print(resultado)
+produto = float(250.50)
+desconto = produto * (15 / 100)
+# o codigo nao reconheceu o %, tive que fazer o
+# calculo de 15/100 para conseguir a porcentagem
+print(f"Valor do produto: R${produto}")
+print(f"Voce esta economizando: R${desconto}")
+produto = produto - desconto
+print(f"Valor final do produto: R${produto}")
+#aqui a sequencia de print usei a f string para puxar dados das strings acima
+
--- PythonProject/questao_8.py
+++ PythonProject/questao_8.py
@@ -1,1 +1,11 @@
-sasassas
+#para palavras apenas usei str, numero inteiro int e quebrados float.
+#ainda tenho uma pequena dificuldade em estruturar os pontos, aspas, etc
+nome=str(input("digite seu nome:"))
+idade=int(input("digite sua idade:"))
+altura=float(input("Digite sua altura (use ponto): "))
+
+
+#nao sei pra que serve o type
+print(type(nome))
+print(type(idade))
+print(type(altura))
